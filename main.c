895a837b5252b4bb6b9ae439516d161a332b9edf
Siddhesh Ballal <83124953+Siddhesh-Ballal@users.noreply.github.com>
2022-01-17 15:14:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 //A106 Siddhesh Ballal
 // Doubly linked List
+/*
+Implement any database using a doubly-linked list with the following options a) Insert a record b) delete a record c) modify a record d) Display list forward Display list backward
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
